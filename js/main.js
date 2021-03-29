cba2946e2f1be4c7e60636604c165c318e4f683d
Mrcubix <tintamare91@gmail.com>
2021-03-29 04:12:00 +0200

    Fixed some bugs and added report generation

---
+++ js/main.js
@@ -0,0 +1,95 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var requests = new XMLHttpRequest();
+    var file = "log.json";
+
+    requests.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            //for (var key in data) {
+            //    console.log(key);
+            //    console.log(data[key]["path"])
+            //    for (var diffkey in data[key]["difficulties"])
+            //        console.log("\t"+diffkey+" : "+data[key][diffkey])
+            //}
+            write_report(data);
+        }
+    };
+
+    requests.open("GET", file, true);
+    requests.send();
+});
+
+function write_report(data) {
+    var body = document.querySelector("body");
+    for (var map in data) {
+        
+
+        var figure = document.createElement("section");
+        figure.setAttribute("style", "background-image: url('./bg/"+map+"/bg.jpg'); background-repeat: no-repeat; background-size: cover;")
+        figure.setAttribute("value", false)
+        figure.addEventListener("click", function() {
+            if (this.getAttribute("value") == "false") {
+                var diff_list = document.createElement("div");
+                for (var diff in data[this.querySelector("p").innerHTML]["difficulties"]) {
+                    var diff_container = document.createElement("div");
+
+                    var diff_text_node = document.createElement("p");
+                    var diff_text = document.createTextNode(diff);
+                    diff_text_node.appendChild(diff_text);
+                    diff_container.appendChild(diff_text_node);
+
+                    var diff_node = document.createElement("span");
+                    var diff_text_node = document.createElement("p");
+                    var diff_text = document.createTextNode(msToTime(data[this.querySelector("p").innerHTML]["difficulties"][diff]));
+                    diff_text_node.appendChild(diff_text);
+                    diff_node.appendChild(diff_text_node);
+                    diff_container.appendChild(diff_node);
+
+                    diff_list.append(diff_container);
+                    this.setAttribute("value", true);
+                }
+                this.appendChild(diff_list);
+            } else {
+                diffs = this.querySelectorAll("p");
+                for (i=Object.keys(data[this.querySelector("p").innerHTML]["difficulties"]).length; i != 0; i--){
+                    this.removeChild(this.querySelector("div"));
+                }
+                this.setAttribute("value", false);
+            }
+        });
+
+        var figcaption = document.createElement("p");
+        var text = document.createTextNode(map);
+        figcaption.appendChild(text);
+
+        var total_time_played_parent_node = document.createElement("span");
+        var total_time_played = 0;
+        for (var difficulty in data[map]["difficulties"]) {
+            total_time_played += data[map]["difficulties"][difficulty]
+        }
+        var total_time_played_node = document.createElement("p");
+        
+        var total_played_text = document.createTextNode("Time played: "+msToTime(total_time_played));
+
+        total_time_played_node.appendChild(total_played_text);
+        total_time_played_parent_node.appendChild(total_time_played_node);
+
+        figure.appendChild(figcaption);
+        figure.appendChild(total_time_played_parent_node);
+
+        body.appendChild(figure);
+    }
+}
+
+function msToTime(duration) {
+    var milliseconds = parseInt((duration % 1000) / 100);
+        seconds = Math.floor((duration / 1000) % 60);
+        minutes = Math.floor((duration / (1000 * 60)) % 60);
+        hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+        hours = (hours < 10) ? "0" + hours : hours;
+
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
 import os
+import filecmp
+import shutil
 import requests
 from time import sleep
 import asyncio
+from PIL import Image
+from http.server import test, SimpleHTTPRequestHandler
+import _thread
 
 old_time = 0
 time_passed = 0
@@ -10,9 +15,31 @@ interval = .3
 Timings = [old_time, time_passed]
 data = None
 
+def runserver(bait1, bait2):
+    test(SimpleHTTPRequestHandler)
+
+_thread.start_new_thread(runserver, (None,None))
+
+async def movebg(bm, songpath):
+    title = bm["metadata"]["artist"] + " - " + bm["metadata"]["title"]
+    if str(bm["set"])+" "+title in os.listdir("bg"):
+        pass
+    else:
+        os.makedirs("bg/"+str(bm["set"])+" "+title)
+        shutil.copy(songpath+"\\"+bm["path"]["full"], os.path.abspath("bg/"+str(bm["set"])+" "+title+"/bg."+bm["path"]["bg"].split(".")[-1]))
+        try:
+            path = os.path.abspath("bg/"+str(bm["set"])+" "+title+"/"+os.listdir(os.path.abspath("bg/"+str(bm["set"])+" "+title))[0])
+            img = Image.open(path)
+            img = img.convert('RGB')
+            img.save(os.path.abspath("bg/"+str(bm["set"])+" "+title+"/bg.jpg"))
+            print("conversion successful")
+        except:
+            pass
+
+
 async def getTimePassed(Timings):
     old_time, time_passed = Timings[0], Timings[1]
-    bm, state = None, None
+    bm, state, songpath = None, None, None
     # try sending POST request to gosumemory web output, GET can't be used here due to the limited size of the output data using that method
     try:
         with requests.post("http://localhost:24050/json") as r:
@@ -20,6 +47,7 @@ async def getTimePassed(Timings):
             # Check if the game is playing while gosumemory is still open
             if "error" not in json:
                 state = json["menu"]["state"]
+                songpath = json["settings"]["folders"]["songs"]
                 bm = json["menu"]["bm"]
                 current_time = json["menu"]["bm"]["time"]["current"]
                 # Check if the player is actually playing the map, if that's the case, time_passed and  current_time - old_time are sumed, also set current time to old_time
@@ -29,15 +57,15 @@ async def getTimePassed(Timings):
                 # Prevent time_passed from increasing due to increasing old_time at intro before 00:00:00 if this check was missing
                 if current_time < 0:
                     old_time = 0
-                return [old_time,time_passed], state, bm
-            return Timings,state,bm
+                return [old_time,time_passed], state, bm, songpath
+            return Timings,state,bm, songpath
     except:
-        return Timings,state,bm
+        return Timings, state, bm, songpath
     
 
 async def SaveToFile(Timings, bm):
     title = bm["metadata"]["artist"] + " - " + bm["metadata"]["title"]
-    title = "'{}'".format(title).replace("'", "")
+    title = str(bm["set"])+" "+"'{}'".format(title).replace("'", "")
     difficulty = bm["metadata"]["difficulty"]
     filepath = __file__.split("main.py")[0]+"log.json"
     # if log file exist, then check if it's size is 0, else, create a new file with the previous map's data
@@ -49,32 +77,37 @@ async def SaveToFile(Timings, bm):
             # if the title of the map is in the loaded data, then check the difficulty name contained in it, else, write the previous map and difficulty data to file
             if title in data:
                 # if the difficulty of the previous played map is in data, then sum time_passed and the time contained in the data for the previous played map, else set the difficulty time to time_passed
-                if bm["metadata"]["difficulty"] in data[title]:
-                    data[title][difficulty] = data[title][difficulty] + Timings[1]
+                if bm["metadata"]["difficulty"] in data[title]["difficulties"]:
+                    data[title]["difficulties"][difficulty] = data[title]["difficulties"][difficulty] + Timings[1]
                 else:
-                    data[title][difficulty] = Timings[1]
+                    data[title]["difficulties"][difficulty] = Timings[1]
             else:
-                data[title] = {difficulty: Timings[1]}
+                data[title] = {"difficulties": {difficulty: Timings[1]}}
             with open(filepath, "w") as f:
-                f.write(json.dumps(data))
+                json.dump(data, f, indent=4)
         else:
 
             with open(filepath, "w") as f:
-                json.dump({title: {difficulty: Timings[1]}}, f)
+                json.dump({title: {"difficulties": {difficulty: Timings[1]}}}, f, indent=4)
     else:
         with open(filepath, "w") as f:
-            json.dump({title: {difficulty: Timings[1]}}, f)
+            json.dump({title: {"difficulties": {difficulty: Timings[1]}}}, f, indent=4)
     print("data write")
 
 running = True
 while running:
     # Check time passed every [interval] passed
-    Timings, state, bm = asyncio.run(getTimePassed(Timings))
+    Timings, state, bm, songpath = asyncio.run(getTimePassed(Timings))
     # Save Time passed to file once player is back to menu if the player actually played part of the map
     if state != 2 and Timings[1] != 0:
         asyncio.run(SaveToFile(Timings, bm))
         # reset time passed on a map to 0 once done
         Timings[1] = 0
+        if os.path.exists("bg"):
+            asyncio.run(movebg(bm, songpath))
+        else:
+            os.mkdir("bg")
+            asyncio.run(movebg(bm, songpath))
     # interval present to avoid high cpu usage 
     sleep(interval)
     
